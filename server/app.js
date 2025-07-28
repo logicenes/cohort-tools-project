84a7d837a85dea730563a2d8381710e63e23db59
„Enes <enes.gnay@yahoo.de>
2025-07-28 18:13:28 +0200

    day one done

--- server/app.js
+++ server/app.js
@@ -2,11 +2,15 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cors = require("cors");
+
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
+const cohorts = require("./cohorts.json");
+const students = require("./students.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -15,6 +19,7 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -28,6 +33,14 @@ app.use(cookieParser());
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
+
+app.get("/api/cohorts",function(request, response, next){
+  response.json(cohorts)
+})
+
+app.get("/api/students",function(request, response, next){
+  response.json(students)
+})
 
 
 // START SERVER
