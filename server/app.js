20e09130a9936500a161234d7f458e24c863ca5d
„Enes <enes.gnay@yahoo.de>
2025-07-29 18:21:09 +0200

    day2

--- server/app.js
+++ server/app.js
@@ -3,18 +3,22 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 const cors = require("cors");
+const mongoose = require("mongoose");
 
+//Import the model
+const Cohort = require("./models/Cohort.model")
+const Student = require("./models/Student.model")
 
-// STATIC DATA
-// Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
-
-const cohorts = require("./cohorts.json");
-const students = require("./students.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
+// connecting to database
+ 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to MongoDB", err));
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -27,6 +31,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
+// GET /cohorts - retrieve all the cohorts
+app.get("/api/cohorts", (req, res) => {
+  Cohort.find({})
+    .then((cohorts) => {
+      console.log("Retrieved cohorts ->", cohorts);
+      res.json(cohorts);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving cohorts ->", error);
+      res.status(500).send({ error: "Failed to retrieve cohorts" });
+    });
+});
+
+// GET /students - retrieve all the students
+app.get("/api/students", (req, res) => {
+  Student.find({})
+    .then((students) => {
+      console.log("Retrieved students ->", students);
+      res.json(students);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving students ->", error);
+      res.status(500).send({ error: "Failed to retrieve students" });
+    });
+});
+
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
@@ -34,13 +64,6 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
-app.get("/api/cohorts",function(request, response, next){
-  response.json(cohorts)
-})
-
-app.get("/api/students",function(request, response, next){
-  response.json(students)
-})
 
 
 // START SERVER
---
+++ server/models/Cohort.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const cohortSchema = new Schema({
+  inProgress: Boolean,
+  cohortSlug: String,
+  cohortName: String,
+  program: String,
+  campus: String,
+  startDate: { type: Date, default: Date.now },
+  endDate:{ type: Date, default: Date.now },
+  programManager: String,
+  leadTeacher: String, 
+  totalHours: Number
+});
+
+const Cohort = mongoose.model("Cohort", cohortSchema);
+
+
+module.exports = Cohort;
